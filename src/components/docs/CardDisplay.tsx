1bd3e8cf0a9e1548b10e80bd91b458929ce7ca62
Martin Heidegger <martin.heidegger@gmail.com>
2025-09-20 11:41:45 +0900

    feat: extracting dir ops and adding toggle for showing card width / without overflow cutting

--- src/components/docs/CardDisplay.tsx
+++ src/components/docs/CardDisplay.tsx
@@ -2,70 +2,33 @@ import type { CardType } from '@/docs/card'
 import type { DocDisplayProps } from '@/components/docs/DocsDisplay'
 import { CardDisplayFront } from '@/components/docs/CardDisplayFront'
 import { CardDisplayBack } from '@/components/docs/CardDisplayBack'
-import { useEffect, useRef, useState } from 'react'
+import { useRef, useState } from 'react'
 import { encode } from '@/utils/buffer'
+import { useDirHandle, writeFile } from '@/hooks/useDirHandle'
+import { useToggle } from '@/hooks/useToggle'
 
-async function writeFile(dir: FileSystemDirectoryHandle, name: string, data: Uint8Array<ArrayBuffer>) {
-    const file = await dir.getFileHandle(name, {
-        create: true
-    })
-    const permission = await file.requestPermission({
-        mode: 'readwrite'
-    })
-    if (permission === "denied") {
-        throw new Error('You denied the download process :-(')
-    }
-    if (permission !== "granted") {
-        return
-    }
-    const frontWrite = await file.createWritable({})
-    try {
-        await frontWrite.write(data)
-    } finally {
-        await frontWrite.close()
-    }
-}
 
 export function CardDisplay({ json, link, docKey }: DocDisplayProps<CardType>) {
-    const isCut = true
+    const [isCut, toggleCut] = useToggle(true)
     const front = useRef<SVGSVGElement>(null)
     const back = useRef<SVGSVGElement>(null)
-    const [disabled, setDisabled] = useState<string | undefined>("Your browser doesn't support the download API")
-    const [error, setError] = useState<Error>()
-    useEffect(() => {
-        if ('showDirectoryPicker' in globalThis) {
-            setDisabled(undefined)
-        }
-    }, [])
-    const download = () => {
-        if (!front.current || !back.current) {
-            return
-        }
-        if (disabled) return
-        setError(undefined)
-        setDisabled('Processing files')
-        const frontSvg = front.current.outerHTML
-        const backSvg = back.current.outerHTML
-        const name = `bc_${docKey}`
-        ;(async () => {
-            const dir = await globalThis.window.showDirectoryPicker()
+    const { disabled, openDir, error } = useDirHandle(
+        async (dir) => {
+            if (!front.current || !back.current) {
+                throw new Error('not ready')
+            }
+            const frontSvg = front.current.outerHTML
+            const backSvg = back.current.outerHTML
+            const name = `bc_${docKey}`
             await writeFile(dir, `${name}_front.svg`, encode(frontSvg))
             await writeFile(dir, `${name}_back.svg`, encode(backSvg))
             alert('Done.')
-        })()
-            .catch(error => {
-                if (error.name === 'AbortError') {
-                    return
-                }
-                setError(error)
-            })
-            .finally(() => {
-                setDisabled(undefined)
-            })
-    }
+        }
+    )
     return <>
         <div>
-            <button title={disabled} onClick={download} disabled={disabled !== undefined}>Download</button>
+            <input type="checkbox" checked={isCut} onClick={toggleCut} />
+            <button title={disabled} onClick={openDir} disabled={disabled !== undefined}>Download</button>
         </div>
         {error ? <div className="error">{String(error)}</div>: null}
         <div>
---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export function useToggle (defaultValue: boolean = false): [
+    value: boolean,
+    toggleValue: (override?: any) => void
+] {
+    const [value, setValue] = useState(defaultValue)
+    const toggleCut = (override: any) => {
+        setValue(
+            current => {
+                if (override === true || override === false) return override
+                return !current
+            }
+        )
+    }
+    return [value, toggleCut]
+}
