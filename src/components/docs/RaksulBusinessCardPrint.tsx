38ad00a0a16acd2e6e1d1e1b9df9c4a1bcf18123
Martin Heidegger <martin.heidegger@gmail.com>
2025-09-28 14:43:47 +0900

    fix: move the images to the /images folder

--- src/components/docs/RaksulBusinessCardPrint.tsx
+++ src/components/docs/RaksulBusinessCardPrint.tsx
@@ -13,24 +13,24 @@ export function RaksulBusinessCardPrint() {
         <p>
           Open the products editor for business card and select an the empty
           template.
-          <img src="/public/screenshot-06-create-new-template.png" />
+          <img src="/images/screenshot-06-create-new-template.png" />
         </p>
         <p>
           Use the normal settings
-          <img src="/public/screenshot-07-size-choices.png" />
+          <img src="/images/screenshot-07-size-choices.png" />
         </p>
         <p>
           Upload the front and back side of the card.
-          <img src="/public/screenshot-08-upload.png" />
+          <img src="/images/screenshot-08-upload.png" />
         </p>
         <p>
           Drag & Drop the front side on the editor and use the "配置" button to
           change the positon. Set it to 96x60 at -2.5/-2.5.
-          <img src="/public/screenshot-09-positioning.png" />
+          <img src="/images/screenshot-09-positioning.png" />
         </p>
         <p>
           Add a page then do the same thing with the background image.
-          <img src="/public/screenshot-10-add-bg.png" />
+          <img src="/images/screenshot-10-add-bg.png" />
         </p>
         <p>
           Now go to order a regular business card. Select the "通常" (tsujo)
@@ -46,22 +46,22 @@ export function RaksulBusinessCardPrint() {
           Use the quality "標準"(hyojun) for and we need it colored on both
           sides. Note: if you are not in a hurry it is no problem to order the
           business cards a few days in advance. they then cost much less!
-          <img src="/public/screenshot-02-add-to-cart.png" />
-          <img src="/public/screenshot-03-selection.png" />
+          <img src="/images/screenshot-02-add-to-cart.png" />
+          <img src="/images/screenshot-03-selection.png" />
         </p>
         <p>
           To upload the designs use the{" "}
-          <img src="/public/screenshot-04-upload-data.png" /> and{" "}
-          <img src="/public/screenshot-05-select-mydesign.png" /> to add our
+          <img src="/images/screenshot-04-upload-data.png" /> and{" "}
+          <img src="/images/screenshot-05-select-mydesign.png" /> to add our
           designs.
         </p>
         <p>
           Use <strong>both</strong> template files you just created.
-          <img src="/public/screenshot-06-use-sides.png" />
+          <img src="/images/screenshot-06-use-sides.png" />
         </p>
         <p>
           Follow through the data check.
-          <img src="/public/screenshot-11-data-check.png" />
+          <img src="/images/screenshot-11-data-check.png" />
         </p>
         <p>Confirm the order! Profit</p>
       </section>
