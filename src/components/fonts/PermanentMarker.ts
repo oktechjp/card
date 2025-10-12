542162624ba0d88f4d34fc243341f017344db8ce
Martin Heidegger <martin.heidegger@gmail.com>
2025-10-13 03:46:45 +0900

    fix: font import

--- src/components/fonts/PermanentMarker.ts
+++ src/components/fonts/PermanentMarker.ts
@@ -8,6 +8,6 @@ export const PermanentMarker = `
   font-family: 'Permanent Marker';
   font-style: normal;
   font-weight: 400;
-  src: url(fonts/a73712ce4f3b32d3c7a1a657d56c6aaf50c719b7fa7fe0541905fe77f5a9906c.ttf) format('truetype');
+  src: url(/fonts/a73712ce4f3b32d3c7a1a657d56c6aaf50c719b7fa7fe0541905fe77f5a9906c.ttf) format('truetype');
 }
 `;
--- src/components/fonts/ShipporiAntiqueB1.ts
+++ src/components/fonts/ShipporiAntiqueB1.ts
@@ -8,6 +8,6 @@ export const ShipporiAntiqueB1 = `
   font-family: 'Shippori Antique B1';
   font-style: normal;
   font-weight: 400;
-  src: url(fonts/f06b41687707a0c00293eca6128db9123bf84631dd45b087365fdfd1ebb88ce7.ttf) format('truetype');
+  src: url(/fonts/f06b41687707a0c00293eca6128db9123bf84631dd45b087365fdfd1ebb88ce7.ttf) format('truetype');
 }
 `;
