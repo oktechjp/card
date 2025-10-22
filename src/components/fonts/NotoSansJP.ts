59c8eb7a72a5ea852f5d3ea49a4f90e972a4274d
Martin Heidegger <martin.heidegger@gmail.com>
2025-10-22 21:47:39 +0900

    cleanup: removing unused font weights

--- src/components/fonts/NotoSansJP.ts
+++ src/components/fonts/NotoSansJP.ts
@@ -1,6 +1,6 @@
 import type { Font } from "./font";
 /**
- * Generated from url=https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900
+ * Generated from url=https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700
  *
  * Generated using -> npm run import:fonts
  */
@@ -10,56 +10,14 @@ export const NotoSansJP = {
 @font-face {
   font-family: 'Noto Sans JP';
   font-style: normal;
-  font-weight: 100;
-  src: url(/fonts/f75b404140cf68757be69e20e1d4522010c18f8b3b4193ca5da3b0f8ddfec0a7.ttf) format('truetype');
-}
-@font-face {
-  font-family: 'Noto Sans JP';
-  font-style: normal;
-  font-weight: 200;
-  src: url(/fonts/0231dc6730eb08c5afdfba924e607a15f904d4e87ec76139bf9bbe83e06af16f.ttf) format('truetype');
-}
-@font-face {
-  font-family: 'Noto Sans JP';
-  font-style: normal;
-  font-weight: 300;
-  src: url(/fonts/fc3e20dbb01c4172e685894255ac756cc21523c549c696e05539a13e173452ae.ttf) format('truetype');
-}
-@font-face {
-  font-family: 'Noto Sans JP';
-  font-style: normal;
   font-weight: 400;
   src: url(/fonts/d55e0216f798d5f93398b2bdbd4b2afd8e3ce912638ace01ddffc34dfa54541b.ttf) format('truetype');
 }
 @font-face {
   font-family: 'Noto Sans JP';
   font-style: normal;
-  font-weight: 500;
-  src: url(/fonts/a105d38020780327bc0ec4dfba87d0582c471f32f2c85e4cfcb47d291e96569e.ttf) format('truetype');
-}
-@font-face {
-  font-family: 'Noto Sans JP';
-  font-style: normal;
-  font-weight: 600;
-  src: url(/fonts/ca32da589823f6e6f38202827efcfef90600f03e9621dde8ce94622b05041c41.ttf) format('truetype');
-}
-@font-face {
-  font-family: 'Noto Sans JP';
-  font-style: normal;
   font-weight: 700;
   src: url(/fonts/bac2cafc9e0464095b9b5e8df6cf36d28c4b0531823a83b294c5899d4ce951e6.ttf) format('truetype');
-}
-@font-face {
-  font-family: 'Noto Sans JP';
-  font-style: normal;
-  font-weight: 800;
-  src: url(/fonts/76c6bd4a9a2cf240a162e395920084eae2cdfed4afad09da32775b66ff4f1063.ttf) format('truetype');
-}
-@font-face {
-  font-family: 'Noto Sans JP';
-  font-style: normal;
-  font-weight: 900;
-  src: url(/fonts/5d2a413852180fdb719b27c7de7abe84de26a8ce9b622a3060f9079c5ebc46ce.ttf) format('truetype');
 }
 `,
 } satisfies Font;
